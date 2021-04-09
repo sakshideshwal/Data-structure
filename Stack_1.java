f7df0181b5990cbbc747e282dbde14534bb167cb
sakshideshwal <sakshideshwal2631@gmail.com>
2021-04-09 13:36:15 +0530

    Stack

---
+++ Stack_1.java
@@ -0,0 +1,18 @@
+import java.util.Stack;
+/*
+stack[10]
+
+top=-1
+
+*/
+class Stack_1{
+
+    public static void main(String[] args) {
+        Stack<Integer> st=new Stack(); 
+        st.push(2);
+        st.push(3);
+
+        System.out.println(st);
+        System.out.println(st.peek());
+    }
+}
